ea2515a2fbd11f0472813670d816689b4c4a4ef9
JD <176695371@qq.com>
2024-05-19 10:40:16 +0800

    modbus帧测试

--- ModbusTest/MainWindow.xaml.cs
+++ ModbusTest/MainWindow.xaml.cs
@@ -1,17 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Ports;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Windows.Markup;
+using YQ.Tool;
+using YQ.Tool.JY;
 
 namespace ModbusTest
 {
@@ -20,15 +14,30 @@ namespace ModbusTest
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            InitCom();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            var data = Encoding.UTF8.GetBytes(code.Text);
-            var crc = CalculateCRC(data, data.Length);
+            
+            var sdata= code.Text;
+            if (sdata.Length % 2 != 0)
+            {
+                sdata.PadLeft(sdata.Length + 1, '0');
+            }
+            List<byte> data = new List<byte>();
+            for (int i = 0; i < sdata.Length; i += 2)
+            {
+                byte b = Convert.ToByte(sdata.Substring(i, 2), 16);
+                data.Add(b);
+            }
+            //var data = Encoding.UTF8.GetBytes(code.Text);
+            //var crc = CMBRTU.CalculateCrc(data.ToArray(), data.Count());
+            var crc = CalculateCrc(data.ToArray(), 0,data.Count());
             var buffer1=BitConverter.GetBytes(crc);
             var str = string.Empty;
             foreach (byte b in buffer1)
@@ -37,16 +46,17 @@ namespace ModbusTest
             }
             Crc.Text = str;
         }
-        public static ushort CalculateCRC(byte[] data, int length)
+        private static ushort CalculateCrc(byte[] data, int start, int length)
         {
             ushort crc = 0xFFFF;
 
-            for (int i = 0; i < length; i++)
+            for (int i = start; i < start + length; i++)
             {
                 crc ^= data[i];
+
                 for (int j = 0; j < 8; j++)
                 {
-                    if ((crc & 0x0001) == 1)
+                    if ((crc & 0x0001) != 0)
                     {
                         crc >>= 1;
                         crc ^= 0xA001;
@@ -59,6 +69,50 @@ namespace ModbusTest
             }
 
             return crc;
+        }
+        private void InitCom()
+        {
+            var com = new ComParamter("COM1-9000-n-8-1");
+            SerialManager.Instance.CreateAndOpenPort(com);
+            if (SerialManager.Instance.PortList.ContainsKey("COM1"))
+            {
+                SerialManager.Instance.PortList["COM1"].DataReceived += ListenInViewModel_DataReceived;
+
+            }
+        }
+        List<byte> ReceiveData = new List<byte>();
+        private void ListenInViewModel_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (e.EventType == SerialData.Chars)
+            {
+                SerialPort port = sender as SerialPort;
+                byte[] buffer = new byte[port.BytesToRead];
+                port.Read(buffer, 0, buffer.Length);
+                ReceiveData.AddRange(buffer);
+                if (ReceiveData.Count<5)
+                {
+                    return;
+                }
+                else
+                {
+                    var Result = ModbusDataPack.Instance.TryPackData(ReceiveData.ToArray());
+                    if (Result != DataPackMetaData.Null)
+                    {
+                        byte[] resbytes = ReceiveData.Skip((int)Result.StartIndex).Take((int)(Result.Length)).ToArray();
+                        ReceiveData.RemoveRange(0, (int)Result.StartIndex + (int)Result.Length);
+                        var str = string.Empty;
+                        foreach (byte b in resbytes)
+                        {
+                            str += b.ToString("X2");
+                        }
+                        this.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            code.Text = str;
+                        }));
+                        
+                    }                    
+                }              
+            }
         }
     }
 }
---
+++ ModbusTest/ModbusDataPack.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using YQ.Tool;
+
+namespace ModbusTest
+{
+    public class ModbusDataPack
+    {
+        private static ModbusDataPack? m_Instance;
+        public static ModbusDataPack Instance
+        {
+            get
+            {
+                if (m_Instance is null)
+                    m_Instance = new ModbusDataPack();
+                return m_Instance;
+            }
+        }
+        public DataPackMetaData TryPackData(IEnumerable<byte> Bytes)
+        {
+            int DataLength = Bytes.Count();
+            for (int i = 0; i < DataLength-5; i++)
+            {
+                int PackLength = CalculateCrc((byte[])Bytes,i,DataLength-i);
+                if (PackLength==0)
+                {
+                    return new DataPackMetaData((uint)i, (uint)(DataLength - i), 0);
+                }
+            }
+            return DataPackMetaData.Null;
+        }
+        /// <summary>
+        /// ModBus校验
+        /// </summary>
+        /// <param name="data"></param>
+        /// <param name="start"></param>
+        /// <param name="length"></param>
+        /// <returns></returns>
+        private static ushort CalculateCrc(byte[] data, int start, int length)
+        {
+            ushort crc = 0xFFFF;
+
+            for (int i = start; i < start + length; i++)
+            {
+                crc ^= data[i];
+
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((crc & 0x0001) != 0)
+                    {
+                        crc >>= 1;
+                        crc ^= 0xA001;
+                    }
+                    else
+                    {
+                        crc >>= 1;
+                    }
+                }
+            }
+
+            return crc;
+        }
+    }
+    
+    
+}
